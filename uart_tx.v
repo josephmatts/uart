bdd5542b6e747468605c5dfd4b49b345b0f5e95c
josephtachlog <joseph@tachlog.com>
2026-02-05 13:01:04 +0530

    uart_tx hardware tested

--- uart_tx.v
+++ uart_tx.v
@@ -1,7 +1,7 @@
 module uart_tx 
 #(
     parameter DATA_WIDTH  = 8,
-    parameter CLK_FREQ    = 50_000_000,
+    parameter CLK_FREQ    = 125_000_000,
     parameter BAUD_RATE   = 9600,
     parameter STOP_BITS   = 1
 )
@@ -11,134 +11,102 @@ module uart_tx
     input wr_en,
     input [DATA_WIDTH-1:0] din,
     
-    output tx,
-    output tx_busy 
+    output reg tx,
+    output     tx_busy 
 );
 
+// Baud timing
 localparam CYCLES_PER_BIT = CLK_FREQ/BAUD_RATE;
 localparam COUNTER_LENGTH = $clog2(CYCLES_PER_BIT+1);
 
-reg [DATA_WIDTH-1:0] data_to_send;
-reg tx_reg;
-
-reg [COUNTER_LENGTH-1:0] cycle_counter;
-reg [3:0] bit_counter;
-
-wire next_bit     = cycle_counter == CYCLES_PER_BIT-1;
-wire payload_done = (bit_counter == DATA_WIDTH-1) && next_bit;
-wire stop_done    = (bit_counter   == STOP_BITS-1) && current_state == STOP;
-
+// timing
 localparam [1:0]
     IDLE  = 2'b00,
     START = 2'b01,
     DATA  = 2'b10,
     STOP  = 2'b11;
+reg [1:0] state;
 
-reg [1:0] current_state, next_state;
+// registers
+reg [DATA_WIDTH-1:0] data_to_send;
+reg [COUNTER_LENGTH-1:0] cycle_counter;
+reg [$clog2(DATA_WIDTH):0] bit_counter;
+reg [1:0] stop_counter;
 
-assign tx_busy = current_state != IDLE;
-assign tx       = tx_reg;
+assign tx_busy = state != IDLE;
 
-// next state logic
-always@(*) begin
-    case(current_state)
-
-        IDLE : begin
-            if(wr_en) begin
-                next_state = START;
-            end else begin
-                next_state = IDLE;
-            end
-        end
-
-        START : begin
-            if(next_bit)  begin
-                next_state = DATA;
-            end else begin
-                next_state = START;
-            end                     
-        end
-        
-        DATA : begin
-            if(payload_done) begin
-                next_state = STOP;
-            end else begin
-                next_state = DATA;
-            end
-        end
-        
-        STOP : begin
-            if(stop_done) begin
-                next_state = IDLE;
-            end else begin
-                next_state = STOP;
-            end
-        end
-        
-        default : begin
-            next_state = IDLE;
-        end
-
-    endcase
-end
-
-// data 
+// uart tx fsm
 always@(posedge clk) begin
     if(rst) begin
-        data_to_send <= {DATA_WIDTH{1'b0}};
-    end else if(current_state == IDLE && wr_en) begin
-        data_to_send <= din;
-    end else if(current_state == DATA && next_bit) begin
-        data_to_send <= {1'b0, data_to_send[DATA_WIDTH-1:1]};
-    end
-end
-
-// bit counter
-always@(posedge clk) begin
-    if(rst) begin
-        bit_counter <= 4'b0;
-    end else if(current_state != next_state) begin
-        bit_counter <= 4'b0;
-    end else if(next_bit && (current_state == DATA || current_state == STOP)) begin
-        bit_counter <= bit_counter + 1;
-    end 
-end
-
-// cycle counter
-always@(posedge clk) begin
-    if(rst) begin
-        cycle_counter <= {COUNTER_LENGTH{1'b0}};
-    end else if(next_bit) begin
-        cycle_counter <= {COUNTER_LENGTH{1'b0}};        
-    end else if(current_state != IDLE) begin
-        cycle_counter <= cycle_counter + 1'b1;
-    end
-end
-
-
-// progress current state
-always@(posedge clk) begin
-    if(rst) begin
-        current_state <= IDLE;
+        state         <= IDLE;
+        cycle_counter <= 0;
+        bit_counter   <= 0;
+        stop_counter  <= 0;
+        data_to_send  <= 0;
+        tx            <= 1'b1;
     end else begin
-        current_state <= next_state;
+        case(state)
+        
+            IDLE : begin
+                tx            <= 1'b1;
+                cycle_counter <= 0;
+                bit_counter   <= 0;
+                stop_counter  <= 0;
+                
+                if(wr_en) begin
+                    data_to_send  <= din;
+                    state         <= START;
+                end                 
+            end
+            
+            START : begin
+                tx <= 0;
+                if(cycle_counter == CYCLES_PER_BIT-1) begin
+                    cycle_counter <= 0;
+                    state         <= DATA;
+                end else begin
+                    cycle_counter <= cycle_counter + 1'b1;
+                end
+            end
+            
+            DATA : begin
+                tx <= data_to_send[0];
+                if(cycle_counter == CYCLES_PER_BIT-1)begin
+                    cycle_counter <= 0;
+                    data_to_send <= data_to_send >> 1;
+                    
+                    if(bit_counter == DATA_WIDTH-1) begin
+                        bit_counter <= 0;
+                        state <= STOP;
+                    end else begin
+                        bit_counter <= bit_counter + 1'b1;
+                    end
+                end else begin
+                    cycle_counter <= cycle_counter + 1'b1;
+                end
+            end
+            
+            STOP : begin
+                tx <= 1'b1;
+                if(cycle_counter == CYCLES_PER_BIT-1) begin
+                    cycle_counter <= 0;
+                    if(stop_counter == STOP_BITS-1) begin
+                        stop_counter <= 0;
+                        state <= IDLE;                        
+                    end else begin
+                        stop_counter <= stop_counter + 1'b1;
+                    end
+                end else begin
+                    cycle_counter <= cycle_counter + 1'b1;
+                end
+            end
+            
+            default : state <= IDLE;
+                     
+        endcase    
     end
 end
 
-// tx value
-always@(posedge clk) begin
-    if(rst) begin
-        tx_reg <= 1'b1;    
-    end else if(current_state == IDLE) begin
-        tx_reg <= 1'b1;
-    end else if(current_state == START) begin
-        tx_reg <= 1'b0;
-    end else if(current_state == DATA) begin
-        tx_reg <= data_to_send[0];
-    end else if(current_state == STOP) begin
-        tx_reg <= 1'b1;
-    end
-end
 
 
 
